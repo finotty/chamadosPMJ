fdb39e31863701cbb34ab628e7880584dfb30110
Hugo Finote <34668254+finotty@users.noreply.github.com>
2025-03-13 15:26:52 -0300

    Update FireBD.js

--- src/ConexaoFirebase/FireBD.js
+++ src/ConexaoFirebase/FireBD.js
@@ -3,14 +3,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABKVXjrjF5n0kIEU50tm8Spxit9zZkcos",
-  authDomain: "chamadospmj.firebaseapp.com",
-  databaseURL: "https://chamadospmj-default-rtdb.firebaseio.com",
-  projectId: "chamadospmj",
-  storageBucket: "chamadospmj.appspot.com",
-  messagingSenderId: "503364785711",
-  appId: "1:503364785711:web:302e0899d31f64013d58da",
-  measurementId: "G-BDR8JDNBP0"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "1",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
